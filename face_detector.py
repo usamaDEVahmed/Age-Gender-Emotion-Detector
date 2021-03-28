824727c842b89f220c13739a2321986638025c2e
usamaDEVahmed <usama.ahmed8565@gmail.com>
2021-03-28 23:30:20 +0500

    added code for face detection

--- face_detector.py
+++ face_detector.py
@@ -1,2 +1,22 @@
 import cv2
+import numpy as np
 
+class FaceDetector():
+
+    def __init__(self):
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    
+
+    def preprocess(self, frame):
+        gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return gray_image
+
+    def detect_faces(self, frame):
+        gray_image = self.preprocess(frame)
+        faces_coords = self.face_cascade.detectMultiScale(gray_image)
+        
+        return faces_coords
+
+    def plot_coords(self, frame, coords):
+        for x, y, w, h in coords:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), thickness=2)
--- initiate.py
+++ initiate.py
@@ -1,13 +1,17 @@
 import cv2
 from video_initializer import VideoInitializer
+from face_detector import FaceDetector
 
 if __name__ == '__main__':
     video_initializer = VideoInitializer()
+    face_detector = FaceDetector()
 
     while True:
         frame = video_initializer.get_frame()
         resized_frame = video_initializer.resize_frame(frame, (640, 480))
-        cv2.imshow('Frame', frame)
+        faces_coords = face_detector.detect_faces(resized_frame)
+        face_detector.plot_coords(resized_frame, faces_coords)
+        cv2.imshow('Face Detector', resized_frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
